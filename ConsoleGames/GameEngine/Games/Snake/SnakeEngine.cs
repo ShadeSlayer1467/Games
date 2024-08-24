b5e39b69505f4fbba2d387bf275323569c7e8100
Matthew Read <matthew.read.101@gmail.com>
2024-08-23 21:02:41 -0700

    SRP + Minor Bug Fix --also added fixed speed between x/y axis

--- ConsoleGames/GameEngine/Games/Snake/SnakeEngine.cs
+++ ConsoleGames/GameEngine/Games/Snake/SnakeEngine.cs
@@ -10,14 +10,15 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace GamePlatform.Games.Snake
 {
     [GameName("Snake")]
     internal class SnakeEngine : ConsoleGame
     {
-        SnakeBody snake;
-        Vector2? food = null;
+        SnakeGameModel snakeModel;
+        Vector2 Border => new Vector2(GameConsoleUI.WindowWidth, GameConsoleUI.WindowHeight);
         bool gameOver = false;
         char lastKeyPressed;
         int sleepMS = 100;
@@ -25,14 +26,14 @@ namespace GamePlatform.Games.Snake
 
         public SnakeEngine()
         {
-            snake = new SnakeBody();
+            snakeModel = new SnakeGameModel();
         }
 
         public override void InitializeGame()
         {
             GameConsoleUI.ClearConsole();
-            snake = new SnakeBody();
-            food = new Vector2(5, 5);
+            snakeModel = new SnakeGameModel();
+            snakeModel.food = new Vector2(5, 5);
             gameOver = false;
         }
         public override void RunGame()
@@ -41,7 +42,10 @@ namespace GamePlatform.Games.Snake
             GameConsoleUI.WriteLine(ENGLISH_DIRECTIONS, COMMUNICATION_LINE_TOP);
             GameConsoleUI.ReadKeyChar(true);
             GameConsoleUI.ClearConsole();
-            GenerateFood();
+            PrintBorder();
+
+            snakeModel.GenerateFood(Border);
+            PrintFood();
 
             do UpdateFrame(); while (!gameOver); GameOver();
         }
@@ -54,18 +58,19 @@ namespace GamePlatform.Games.Snake
         private void GameOver()
         {
             GameConsoleUI.ClearConsole();
-            GameConsoleUI.WriteLine($"Game Over... Size: {snake.Body.Count}... Press space to continue");
+            GameConsoleUI.WriteLine($"Game Over... Size: {snakeModel.Body.Count}... Press space to continue");
+            Thread.Sleep(1000);
             GameConsoleUI.ReadKeyChar(true);
         }
         private void UpdateFrame()
         {
-            Thread.Sleep(sleepMS);
             if (GameConsoleUI.KeyAvailable)
             {
                 lastKeyPressed = GetMoveDirection();
                 MovePieces(lastKeyPressed);
             }
-            Vector2 tail = snake.Body.Last();
+            Thread.Sleep(sleepMS);
+            Vector2 tail = snakeModel.Body.Last();
             if (CheckForCollision())
             {
                 gameOver = true;
@@ -73,14 +78,16 @@ namespace GamePlatform.Games.Snake
             }
             if (CheckFoodCollision())
             {
-                food = null;
-                GenerateFood();
-                snake.AddToBody();
+                snakeModel.AddToBody();
                 PrintSnake();
+
+                snakeModel.food = null;
+                snakeModel.GenerateFood(Border);
+                PrintFood();
             }
             else
             {
-                snake.Move();
+                snakeModel.Move();
                 PrintSnake();
                 GameConsoleUI.SetCursorPosition((int)tail.X, (int)tail.Y);
                 GameConsoleUI.Write(" ");
@@ -88,39 +95,44 @@ namespace GamePlatform.Games.Snake
         }
         private void PrintSnake()
         {
-            foreach (Vector2 piece in snake.Body)
+            foreach (Vector2 piece in snakeModel.Body)
             {
                 GameConsoleUI.SetCursorPosition((int)piece.X, (int)piece.Y);
                 GameConsoleUI.Write("O");
             }
         }
-        private void GenerateFood()
+        private void PrintFood()
         {
-            Random random = RandomSingleton.Instance;
-
-            Vector2 windowSize = new Vector2(GameConsoleUI.WindowWidth, GameConsoleUI.WindowHeight);
-            while (true)
-            {
-                food = new Vector2(random.Next(1, (int)windowSize.X), random.Next(1, (int)windowSize.Y));
-                if (!snake.Body.Contains(food.Value)) break;
-            }
-            GameConsoleUI.SetCursorPosition((int)food.Value.X, (int)food.Value.Y);
+            GameConsoleUI.SetCursorPosition((int)snakeModel.food.Value.X, (int)snakeModel.food.Value.Y);
             GameConsoleUI.Write("X");
+        }
+        private void PrintBorder()
+        {
+            (int width, int height) = ((int)Border.X, (int)Border.Y);
+            for (int i = 0; i < width; i++)
+            {
+                GameConsoleUI.SetCursorPosition(i, 0);
+                GameConsoleUI.Write("█");
+                GameConsoleUI.SetCursorPosition(i, height - 1);
+                GameConsoleUI.Write("█");
+            }
+            for (int i = 0; i < height; i++)
+            {
+                GameConsoleUI.SetCursorPosition(0, i);
+                GameConsoleUI.Write("██");
+                GameConsoleUI.SetCursorPosition(width - 2, i);
+                GameConsoleUI.Write("██");
+            }
         }
         private bool CheckForCollision()
         {
-            Vector2 windowSize = new Vector2(GameConsoleUI.WindowWidth, GameConsoleUI.WindowHeight);
-            if (snake.Head.X == windowSize.X - 1 || snake.Head.Y == windowSize.Y - 1 || snake.Head.X == 0 || snake.Head.Y == 0) return true;
-            if (snake.Body.Count > 1 && snake.Body.Skip(1).Any(b => b == snake.Head)) return true;
+            if (snakeModel.Head.X == Border.X - 2 || snakeModel.Head.Y == Border.Y - 1 || snakeModel.Head.X == 1 || snakeModel.Head.Y == 1) return true;
+            if (snakeModel.Body.Count > 1 && snakeModel.Body.Skip(1).Any(b => b == snakeModel.Head)) return true;
             return false;
         }
         private bool CheckFoodCollision()
         {
-            if (snake.Head == food)
-            {
-                return true;
-            }
-            return false;
+            return (snakeModel.Head == snakeModel.food);
         }
         private char GetMoveDirection()
         {
@@ -139,60 +151,28 @@ namespace GamePlatform.Games.Snake
             switch (direction)
             {
                 case var t when t == QWERTY_DEFAULT_DIRECTION_KEYS.t:
-                    snake.Direction = new Vector2(0, -1);
+                    snakeModel.Direction = new Vector2(0, -1);
+                    if (snakeModel.Direction.Y == -1) sleepMS = FRAME_WAIT;
                     break;
                 case var t when t == QWERTY_DEFAULT_DIRECTION_KEYS.l:
-                    snake.Direction = new Vector2(-1, 0);
+                    snakeModel.Direction = new Vector2(-1, 0);
+                    if (snakeModel.Direction.X == -1) sleepMS = FRAME_WAIT / 2;
                     break;
                 case var t when t == QWERTY_DEFAULT_DIRECTION_KEYS.d:
-                    snake.Direction = new Vector2(0, 1);
+                    snakeModel.Direction = new Vector2(0, 1);
+                    if (snakeModel.Direction.Y == 1) sleepMS = FRAME_WAIT;
                     break;
                 case var t when t == QWERTY_DEFAULT_DIRECTION_KEYS.r:
-                    snake.Direction = new Vector2(1, 0);
+                    snakeModel.Direction = new Vector2(1, 0);
+                    if (snakeModel.Direction.X == 1) sleepMS = FRAME_WAIT / 2;
                     break;
             }
         }
 
         const int COMMUNICATION_LINE_TOP = 0;
+        const int FRAME_WAIT = 100;
         const string ENGLISH_DIRECTIONS = "Use WASD to move the snake.... press space to continue";
 
         private readonly (char t, char l, char d, char r) QWERTY_DEFAULT_DIRECTION_KEYS = ('w', 'a', 's', 'd');
-    }
-    internal class SnakeBody
-    {
-        public List<Vector2> Body { get; set; }
-        private Vector2 direction;
-        public Vector2 Direction
-        {
-            get { return direction; }
-            set
-            {
-                if (value.X != 0 && value.Y != 0) throw new Exception("Invalid Direction"); ;
-
-                if (value.X != 0 && direction.X == 0) direction = value;
-                else if (value.Y != 0 && direction.Y == 0) direction = value;
-            }
-        }
-        public Vector2 Head => Body[0];
-        public Vector2 Tail => Body[Body.Count - 1];
-        public SnakeBody()
-        {
-            Body = new List<Vector2> { new Vector2(10, 10) };
-            Direction = new Vector2(1, 0);
-        }
-        public void Move()
-        {
-            for (int i = Body.Count - 1; i > 0; i--)
-            {
-                Body[i] = Body[i - 1];
-            }
-            Body[0] += Direction;
-        }
-        public void AddToBody()
-        {
-            var tail = Tail;
-            Move();
-            Body.Add(tail);
-        }
     }
 }
